6737d73d5a73a3d83f38abf8522ff04669abc24e
Bhupendra Kathayat <niko@kali.kali>
2024-08-26 22:16:05 -0400

    created ui for feature 1

--- resources/js/Components/Feature.jsx
+++ resources/js/Components/Feature.jsx
@@ -1,42 +1,72 @@
-import React from 'react';
+import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout.jsx";
-import {Head, usePage} from "@inertiajs/react";
+import { Head, usePage } from "@inertiajs/react";
 
-const Feature = ({feature, answer, children}) => {
-    const {auth} = usePage().props;
+const Feature = ({ feature, answer, children }) => {
+    const { auth } = usePage().props;
     const availableCredits = auth.user.available_credits;
+    const isLocked =
+        availableCredits !== null &&
+        feature.required_credits > availableCredits;
+
     return (
         <Authenticated user={auth.user} header={feature.name}>
-            <Head title={"Feature 1"}/>
+            <Head title={"Feature 1"} />
 
-            <div>
-                {
-                    answer !== null && (
-                        <>
-                            Answer: {answer}
-                        </>
-                    )
-                }
-                <div>
-                    {
-                        availableCredits !== null && feature.required_credits > availableCredits && (
-                            <>
-                                You have {availableCredits} credits (locked)
-                            </>
-                        )
-                    }
-                    <div>
-                        <p>
-                            {feature.description}
-                        </p>
-                        <p>
-                            Requires {feature.required_credits} credits
-                        </p>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-5">
+                {answer !== null && (
+                    <div className="bg-green-600 p-2 mb-2 rounded text-white">
+                        Answer: {answer}
                     </div>
-                    {children}
+                )}
+                <div>
+                    {availableCredits !== null && isLocked && (
+                        <div className="text-red-400 font-semibold">
+                            You have {availableCredits} credits left!
+                        </div>
+                    )}
+                    <div className="flex justify-between">
+                        <p>{feature.description}</p>
+                        <p>Requires {feature.required_credits} credits</p>
+                    </div>
+                    <div
+                        className={`mt-6 rounded relative ${
+                            isLocked ? "bg-gray-300" : "bg-gray-300"
+                        }`}
+                    >
+                        {isLocked && (
+                            <div className="absolute w-full h-full flex flex-col items-center justify-center">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                                    />
+                                </svg>
+                                <a href="#" className="mt-2">
+                                    Buy Credits
+                                </a>
+                            </div>
+                        )}
+                        <div
+                            className={`${
+                                isLocked
+                                    ? "pointer-events-none  opacity-30"
+                                    : ""
+                            }`}
+                        >
+                            {children}
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </Authenticated>
     );
 };
--- resources/js/Pages/Feature1/Index.jsx
+++ resources/js/Pages/Feature1/Index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import {useForm} from "@inertiajs/react";
+import React from "react";
+import { useForm } from "@inertiajs/react";
 import Feature from "@/Components/Feature.jsx";
 import PrimaryButton from "@/Components/PrimaryButton.jsx";
 
-const Index = ({feature, answer}) => {
-
-    const {data, setData, post, reset, errors, processing} = useForm({
+const Index = ({ feature, answer }) => {
+    const { data, setData, post, reset, errors, processing } = useForm({
         number1: "",
         number2: "",
     });
@@ -19,26 +18,35 @@ const Index = ({feature, answer}) => {
 
     return (
         <Feature feature={feature} answer={answer}>
-            <form onSubmit={submit}>
-                <div>
-                    <input
-                        type="text"
-                        name="number1"
-                        value={data.number1}
-                        onChange={(e) => setData("number1", e.target.value)}
-                        className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                    />
+            <form onSubmit={submit} className="pt-10 pb-10 pr-2 pl-2">
+                <div className="flex gap-20">
+                    <div className="flex flex-col w-full">
+                        <label htmlFor="number1">First Number</label>
+                        <input
+                            type="text"
+                            name="number1"
+                            id="number2"
+                            value={data.number1}
+                            onChange={(e) => setData("number1", e.target.value)}
+                            className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        />
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <label htmlFor="number2">Second Number</label>
+                        <input
+                            type="text"
+                            name="number2"
+                            id="number2"
+                            value={data.number2}
+                            onChange={(e) => setData("number2", e.target.value)}
+                            className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                        />
+                    </div>
                 </div>
-                <div>
-                    <input
-                        type="text"
-                        name="number2"
-                        value={data.number2}
-                        onChange={(e) => setData("number2", e.target.value)}
-                        className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                    />
-                </div>
-                <PrimaryButton className="mt-4" disabled={processing}>Submit</PrimaryButton>
+
+                <PrimaryButton className="mt-4" disabled={processing}>
+                    Submit
+                </PrimaryButton>
             </form>
         </Feature>
     );
